d5d4013a662b5f03f32dac15c8b82088728f99db
Kimberly MB <k.mueses@yahoo.com>
2022-10-12 12:25:26 -0400

    First commit, creates basic file structure

---
+++ app.js
@@ -0,0 +1,14 @@
+console.log('connnected');
+
+let favNum = prompt('Enter your favorite number!');
+favNum = Number(favNum);
+
+if (favNum >= 1000){
+    alert("Woah, that's a big number!");
+}else if (favNum >= 0){
+    alert("That's a cool number.");
+}else if (favNum < 0){
+    alert("Negative numbers?! That's just bananas.");
+}else (favNum => "");{
+    alert("Enter a valid integer");
+}
